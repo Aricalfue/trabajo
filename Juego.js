f40d98dad254ce38e37fe214bf4728c0dd9737ea
Aricalfue <161866372+Aricalfue@users.noreply.github.com>
2024-03-22 12:52:38 -0500

    Ultimo cambio

--- Juego.js
+++ Juego.js
@@ -17,8 +17,6 @@ let mostrarTiempo = document.getElementById('t-restante')
 
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]
 numeros = numeros.sort(()=>{return Math.random()-0.5})
-console.log(numeros);
-
 
 function contadorTiempo(){
     tiempoRegresivo = setInterval(()=>{
@@ -50,8 +48,6 @@ function voltear(id){
     }
 
     tarjetasDestapadas++
-    console.log(tarjetasDestapadas)
-
     if(tarjetasDestapadas == 1){
         tarjeta1 = document.getElementById(id)
         primerResultado = numeros[id]
